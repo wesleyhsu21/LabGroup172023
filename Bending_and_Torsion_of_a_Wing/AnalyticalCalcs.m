56c888ccb98864a2bf1c4a9986afd512f3c6f8c0
Ko Jai Hyung <thk21@ic.ac.uk>
2023-11-10 23:01:02 +0000

    Theoretical Plots

--- Bending_and_Torsion_of_a_Wing/AnalyticalCalcs.m
+++ Bending_and_Torsion_of_a_Wing/AnalyticalCalcs.m
@@ -7,7 +7,7 @@ syms q1 q2;
 dtdz1 = 1./(2*G*A1).*(q1./0.64*318 + (q1 - q2)./1.7*81);
 dtdz2 = 1./(2*G*A2).*(q2./1.7*81 + q2./1.59*81 + q2./2.34*17.4*2 + q2./0.64*286.66*2 - q1./1.7*81);
 
-eqn1 = 2*A1.*q1 + 2*A2.*q2 == 10000;
+eqn1 = 2*A1.*q1 + 2*A2.*q2 == 222.4; %max loadiing 50 pounds
 eqn2 = dtdz1 - dtdz2 == 0;
 
 [A,B] = equationsToMatrix([eqn1, eqn2], [q1, q2]);
@@ -25,7 +25,24 @@ g76 = g23;
 g87 = g12;
 
 
+%Torque vs shear strain theoretical plots
 
+figure(1) %%UPPER Surface
+plot([0 g23],[0 224.4*0.30406],'r',LineWidth=1.5)
+legend('Torque vs Shear Strain (Upper Surface)')
+xlabel('Shear Strain')
+ylabel('Torque PL')
 
+figure(2) %Lower surface
+plot([0 g76],[0 224.4*0.30406],'r',LineWidth=1.5)
+legend('Torque vs Shear Strain (Lower Surface)')
+xlabel('Shear Strain')
+ylabel('Torque PL')
+
+figure(3) %1-8
+plot([0 g18i],[0 224.4*0.30406],'r',LineWidth=1.5)
+legend('Torque vs Shear Strain (side 1-8)')
+xlabel('Shear Strain')
+ylabel('Torque PL')
 
 
