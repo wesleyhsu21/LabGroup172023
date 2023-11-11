de4bd5ae3c79a2760d36509bc27534b7a534c103
Ko Jai Hyung <thk21@ic.ac.uk>
2023-11-11 14:58:16 +0000

    Fix

--- Bending_and_Torsion_of_a_Wing/AnalyticalCalcs.m
+++ Bending_and_Torsion_of_a_Wing/AnalyticalCalcs.m
@@ -7,7 +7,8 @@ syms q1 q2;
 dtdz1 = 1./(2*G*A1).*(q1./0.64*318 + (q1 - q2)./1.7*81);
 dtdz2 = 1./(2*G*A2).*(q2./1.7*81 + q2./1.59*81 + q2./2.34*17.4*2 + q2./0.64*286.66*2 - q1./1.7*81);
 
-eqn1 = 2*A1.*q1 + 2*A2.*q2 == 222.4; %max loadiing 50 pounds
+Torque = 222.4*0.30406; %torque due to max load 50
+eqn1 = 2*A1.*q1 + 2*A2.*q2 == Torque; 
 eqn2 = dtdz1 - dtdz2 == 0;
 
 [A,B] = equationsToMatrix([eqn1, eqn2], [q1, q2]);
@@ -28,19 +29,19 @@ g87 = g12;
 %Torque vs shear strain theoretical plots
 
 figure(1) %%UPPER Surface
-plot([0 g23],[0 224.4*0.30406],'r',LineWidth=1.5)
+plot([0 g23],[0 Torque],'r',LineWidth=1.5)
 legend('Torque vs Shear Strain (Upper Surface)')
 xlabel('Shear Strain')
 ylabel('Torque PL')
 
 figure(2) %Lower surface
-plot([0 g76],[0 224.4*0.30406],'r',LineWidth=1.5)
+plot([0 g76],[0 Torque],'r',LineWidth=1.5)
 legend('Torque vs Shear Strain (Lower Surface)')
 xlabel('Shear Strain')
 ylabel('Torque PL')
 
-figure(3) %1-8
-plot([0 g18i],[0 224.4*0.30406],'r',LineWidth=1.5)
+figure(3) %1-8 front spar wall
+plot([0 g18i],[0 Torque],'r',LineWidth=1.5)
 legend('Torque vs Shear Strain (side 1-8)')
 xlabel('Shear Strain')
 ylabel('Torque PL')
