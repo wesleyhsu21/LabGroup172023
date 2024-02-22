62ac17d269c3b5b654bef704a634e881a0404ea4
Wesley Hsu <wesley.hsu21@imperial.ac.uk>
2024-02-22 21:43:51 +0000

    cooking on coals

---
+++ Control_Lab Good/LQR_vel/work/slprj/sim/flightControlSystem/flightControlSystem_capi.h
@@ -0,0 +1,7 @@
+#ifndef RTW_HEADER_flightControlSystem_capi_h_
+#define RTW_HEADER_flightControlSystem_capi_h_
+#include "flightControlSystem.h"
+#include "flightControlSystem_private.h"
+extern void flightControlSystem_InitializeDataMapInfo ( ipf5ube4r0 * const
+accn4cnket , void * sysRanPtr , int contextTid ) ;
+#endif
